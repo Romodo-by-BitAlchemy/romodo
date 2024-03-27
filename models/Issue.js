506dcd9a4ac8c127e89431cd9c3fe62cfaf1d13c
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-03-28 04:53:09 +0530

    Add new models and update existing models
    Co-authored-by: JClerve <joesonclerve9@gmail.com>

--- models/Issue.js
+++ models/Issue.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
 
+const IssuesSchema = new mongoose.Schema({
+	dateOccurred: Date,
+	typeDescriptions: String,
+	locations: String,
+	cause: String,
+	repairCost: Number,
+	repairDate: Date,
+	affectedComponents: [String],
+});
+
+const Trip = mongoose.model("Trip", TripSchema);
+module.exports = Trip;
