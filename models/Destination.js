6e620c4af50e2e1af698fa9d6566f0a8a291613a
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-06-16 09:37:46 +0530

    chore: Update Passenger and Destination models with required fields

--- models/Destination.js
+++ models/Destination.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const Passenger = require("./Passenger");
 
+/**
+ * Represents a destination in the system.
+ *
+ * @typedef {Object} DestinationSchema
+ * @property {string} destinationName - The name of the destination.
+ * @property {string} placeId - The ID of the place.
+ * @property {mongoose.Schema.Types.ObjectId} passenger - The ID of the passenger associated with the destination.
+ */
+
 const DestinationSchema = new mongoose.Schema({
 	destinationName: {
 		type: String,
--- models/Route.js
+++ models/Route.js
@@ -3,6 +3,17 @@ const Destination = require("./Destination");
 const LocationSchema = require("./Location");
 
 // Defining Routes schema
+/**
+ * Represents the schema for a route.
+ *
+ * @typedef {Object} RouteSchema
+ * @property {Date} date - The date of the route.
+ * @property {string} startTime - The start time of the route in ISO 8601 format: "HH:MM:SS".
+ * @property {string} [endTime] - The end time of the route in ISO 8601 format: "HH:MM:SS".
+ * @property {LocationSchema} startLocation - The starting location of the route.
+ * @property {Destination[]} destinations - An array of destinations for the route.
+ */
+
 const RouteSchema = new mongoose.Schema({
 	date: {
 		type: Date,
