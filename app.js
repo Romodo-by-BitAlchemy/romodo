b6e6047e8d966e5fce10f40976acb28ad568301e
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-06-18 22:09:39 +0530

    chore: Update app.js to include CORS middleware and fleetRoutingRoutes

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var fleetRoutingRoutes = require("./routes/fleetRouting");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors({ origin: "*" }));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api/fleet", fleetRoutingRoutes);
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+// Start the server and keep it running
+app.listen(PORT, function () {
+	console.log(`Server is running on port ${PORT}`);
+});
