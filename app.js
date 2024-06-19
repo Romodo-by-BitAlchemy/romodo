14b12da92762a0a575497a7781c26bd18041860b
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-06-19 13:51:25 +0530

    chore: Add app-root-path and winston dependencies, update node dependencies

--- app.js
+++ app.js
@@ -3,20 +3,19 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
-var logger = require("morgan");
+var loggerMorgan = require("morgan");
 const cors = require("cors");
-
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var fleetRoutingRoutes = require("./routes/fleetRouting");
-
+var loggerWinston = require("./utils/logger");
 
 var app = express();
 
 app.use(cors({ origin: "*" }));
 
-app.use(logger("dev"));
+app.use(loggerMorgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -29,8 +28,8 @@ app.use("/api/fleet", fleetRoutingRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-
 // Start the server and keep it running
 app.listen(PORT, function () {
 	console.log(`Server is running on port ${PORT}`);
+	loggerWinston.info(`Server is running on port ${PORT}`);
 });
