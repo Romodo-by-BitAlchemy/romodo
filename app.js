7684614f692021f9ac6158a5480560bbb2d43005
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-06-19 10:17:14 +0530

    chore: Add bodyParser middleware for JSON parsing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 const cors = require("cors");
 
@@ -16,6 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
