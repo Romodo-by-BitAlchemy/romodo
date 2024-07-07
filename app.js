65a04b98a578f5bf25e2ac376c244ecc1315af7c
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-07-07 10:52:47 +0000

    chore: Update npm dependencies and add new features

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const loggerMorgan = require("morgan");
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const indexRouter = require("./routes/index");
-const fleetRoutingRoutes = require("./routes/fleetRouting");
-const loggerWinston = require("./utils/logger");
-const vehicleRouter = require("./routes/vehicle");
-const driverRoute = require("./routes/driver");
-const userRoute = require("./routes/user");
-const passengerRoute = require("./routes/passenger");
-const errorHandler = require("./middlewares/ErrorHandler");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors({ origin: "*" }));
-app.use(loggerMorgan("dev"));
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
-// Load environment variables from .env file
-dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Connect to database
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGODB_CONN_STRING)
   .then(() => console.log("Connected to db"))
   .catch((er) => console.log(er));
-
-const app = express();
 
 // Middleware
 app.use(cors());
@@ -49,12 +27,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Import routes
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const fleetRoutingRoutes = require("./routes/fleetRouting");
-const vehicleRoute = require("./routes/vehicle.route");
-const driverRoute = require("./routes/driver.route");
-const userRoute = require("./routes/user.route");
-const passengerRoute = require("./routes/passenger.route");
+const vehicleRoute = require("./routes/vehicle");
+const driverRoute = require("./routes/driver");
+const userRoute = require("./routes/user");
+const passengerRoute = require("./routes/passenger");
 const vehicleRoutes = require("./routes/vehicleDashboardRoutes");
 const driverRoutes = require("./routes/driverDashboardRoutes");
 const tripRoutes = require("./routes/tripDashboardRoutes");
@@ -68,14 +45,6 @@ const issueRouteReport = require('./routes/issueReportRoutes');
 // Routes
 app.use("/", indexRouter);
 app.use("/api/fleet", fleetRoutingRoutes);
-app.use("/api/v1/vehicle", vehicleRouter);
-app.use("/api/v1/driver", driverRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/passenger", passengerRoute);
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-	console.log(`[server]: Server is running at http://localhost:${PORT}`);
 app.use("/api/v1/vehicle", vehicleRoute);
 app.use("/api/v1/driver", driverRoute);
 app.use("/api/v1/user", userRoute);
@@ -94,11 +63,7 @@ app.use('/api/issues', issueRouteReport);
 const errorHandler = require("./middlewares/ErrorHandler");
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-module.exports = app;
-}
